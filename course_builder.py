be3fb110383a554929a0f313a37c4c76f4e01516
Malek Jerbi <malek.jerbi@gmail.Com>
2024-02-27 19:10:42 -0500

    merge graphs

---
+++ course_builder.py
@@ -0,0 +1,48 @@
+from rdflib import Graph, Literal, RDF, URIRef, Namespace
+from rdflib.namespace import FOAF, XSD, RDFS
+import pandas as pd
+
+
+FOCU = Namespace("http://focu.io/schema#")
+FOCUDATA = Namespace("http://focu.io/data#")
+
+df_courses = pd.read_csv("data/CATALOG.csv")
+# NOTE: we are completely ignoring the rows that don't have a course code or course number
+df_courses = df_courses.dropna(subset=["Course code", "Course number"])
+df_course_components = pd.read_csv("data/CU_SR_OPEN_DATA_CATALOG.csv", encoding="utf-16")
+
+
+def build_courses():
+    g = Graph()
+    g.bind("focu", FOCU)
+    g.bind("focudata", FOCUDATA)
+
+    # Build Courses graph
+    for index, row in df_courses.iterrows():
+        code = row["Course code"].strip()
+        number = row["Course number"].strip().split(" ")[0]
+        if code == "" or number == "":
+            continue
+
+        #course_uri = FOCUDATA[row["Key"]]
+        course_uri = FOCUDATA[f"{code}_{number}"]
+        title = row["Title"]
+        description = row["Description"]
+        credits = None
+
+        # NOTE: this could be optimized by merging the two dataframes at init
+        course_component = df_course_components[
+            (df_course_components["Subject"] == code) & (df_course_components["Catalog"] == number)
+        ]["Class Units"]
+        if not course_component.empty:
+            credits = course_component.iloc[0]
+
+        g.add((course_uri, RDF.type, FOCU.Course))
+        g.add((course_uri, FOCU.courseName, Literal(title)))
+        g.add((course_uri, FOCU.courseSubject, Literal(code)))
+        g.add((course_uri, FOCU.courseNumber, Literal((number))))
+        if credits:
+            g.add((course_uri, FOCU.courseCredits, Literal(credits, datatype=XSD.float)))
+        g.add((course_uri, FOCU.courseDescription, Literal(description)))
+    
+    return g
--- graph_builder.py
+++ graph_builder.py
@@ -1,55 +1,20 @@
-import csv
-from rdflib import Graph, Literal, RDF, URIRef, Namespace
-from rdflib.namespace import FOAF, XSD, RDFS
 import os
-import pandas as pd
+from rdflib import Graph
+from course_builder import build_courses
+from lecture_builder import build_lectures
 
-FOCU = Namespace("http://focu.io/schema#")
-FOCUDATA = Namespace("http://focu.io/data#")
 
-df_courses = pd.read_csv("data/CATALOG.csv")
-# NOTE: we are completely ignoring the rows that don't have a course code or course number
-df_courses = df_courses.dropna(subset=["Course code", "Course number"])
-df_course_components = pd.read_csv("data/CU_SR_OPEN_DATA_CATALOG.csv", encoding="utf-16")
+
 
 
 def build_graph():
     g_model = Graph()
     g_model.parse("model.ttl")
 
-    g = Graph()
-    g.bind("focu", FOCU)
-    g.bind("focudata", FOCUDATA)
+    g_courses = build_courses()
+    g_lectures = build_lectures()
 
-    # Build Courses graph
-    for index, row in df_courses.iterrows():
-        code = row["Course code"].strip()
-        number = row["Course number"].strip().split(" ")[0]
-        if code == "" or number == "":
-            continue
-
-        #course_uri = FOCUDATA[row["Key"]]
-        course_uri = FOCUDATA[f"{code}_{number}"]
-        title = row["Title"]
-        description = row["Description"]
-        credits = None
-
-        # NOTE: this could be optimized by merging the two dataframes at init
-        course_component = df_course_components[
-            (df_course_components["Subject"] == code) & (df_course_components["Catalog"] == number)
-        ]["Class Units"]
-        if not course_component.empty:
-            credits = course_component.iloc[0]
-
-        g.add((course_uri, RDF.type, FOCU.Course))
-        g.add((course_uri, FOCU.courseName, Literal(title)))
-        g.add((course_uri, FOCU.courseSubject, Literal(code)))
-        g.add((course_uri, FOCU.courseNumber, Literal((number))))
-        if credits:
-            g.add((course_uri, FOCU.courseCredits, Literal(credits, datatype=XSD.float)))
-        g.add((course_uri, FOCU.courseDescription, Literal(description)))
-
-    g_final = g_model + g
+    g_final = g_model + g_courses + g_lectures
 
     output_dir = "output"
     if not os.path.exists(output_dir):
